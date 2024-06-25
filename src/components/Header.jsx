19e43eb90f9a880dc6e933b2d4041d2ec6d7394e
Zee Jay <zj.bulawan@gmail.com>
2024-06-25 19:59:25 +0800

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <img className="header--logo" src="./images/airbnb-logo.png" alt="airbnb-logo"/>
+    </header>
+  )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <div className="hero">
+      <figure className="hero--image">
+        <img src="./images/photo-grid.png" alt="" />
+      </figure>
+      <h1 className="hero--heading">Online Experiences</h1>
+      <p className="hero--description">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+      </p>
+    </div>
+  )
+}
